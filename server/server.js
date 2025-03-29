0e73a35fb3a2a4cbc81957a3a2ec95df42c4742b
Ronvic2303 <58205103+ronvic12@users.noreply.github.com>
2025-03-28 23:21:07 -0700

    added vercel environment for separating env keys with prod vs dev

--- server/server.js
+++ server/server.js
@@ -9,4 +9,13 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api',router);
-app.listen(port, () => console.log(`Server running on port ${port}`));
+
+if (process.env.VERCEL_ENV === 'production') {
+    // Use this in production mode
+    module.exports = (req, res) => {
+      app(req, res); // Delegate the request to Express
+    };
+  } else if(process.env.VERCEL_ENV === 'development'){
+    // Use this in development mode
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+}  
