20cabe77a299ca9c71861e85197b417af0c1f499
Ronvic2303 <58205103+ronvic12@users.noreply.github.com>
2025-03-29 00:15:45 -0700

    added preview in vercel

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 4000;
+
 const {router} = require('./routers/index')
 require('dotenv').config();
 
@@ -10,12 +10,13 @@ app.use(express.json());
 
 app.use('/api',router);
 
-if (process.env.VERCEL_ENV === 'production') {
-    // Use this in production mode
+if (process.env.VERCEL_ENV === 'production' || process.env.VERCEL_ENV === 'preview') {
+    // Vercel will handle routing in serverless environments in production or preview
     module.exports = (req, res) => {
-      app(req, res); // Delegate the request to Express
+      app(req, res); // Delegate the request to Express handler
     };
-  } else if(process.env.VERCEL_ENV === 'development'){
-    // Use this in development mode
+  } else if (process.env.VERCEL_ENV === 'development') {
+    // This is for local development when you're running the app using `vercel dev`
+    const port = 4000;
     app.listen(port, () => console.log(`Server running on port ${port}`));
-}  
+  }
